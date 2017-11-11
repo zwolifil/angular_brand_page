7e916438b1f3daf92a8ec3f23b778175fa6d5098
zwolifil <s160739@student.pg.gda.pl>
2017-11-11 16:19:34 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {LanguageService} from './language.service';
+import {NavbarService} from "./navbar.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../css/creative.min.css',
+  '../vendor/bootstrap/css/bootstrap.min.css',
+  '../vendor/font-awesome/css/font-awesome.min.css',
+  '../vendor/magnific-popup/magnific-popup.css'],
+  providers: [LanguageService, NavbarService]
 })
 export class AppComponent {
   title = 'app';
+
+  constructor( public languageService: LanguageService) {}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,88 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes} from '@angular/router';
+import { CollapseModule} from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+import { TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import { TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CareerComponent } from './career/career.component';
+import { WorkComponent} from './work/work.component';
+import { QueryRequestComponent} from './query-request/query-request.component';
+import { ProductsComponent } from './products/products.component';
+import { MucountComponent } from './products/mucount/mucount.component';
+import { WildfishComponent } from './products/wildfish/wildfish.component';
+import { SensorsComponent } from './products/sensors/sensors.component';
+import { SoftwareComponent } from './products/software/software.component';
+import { InfplComponent } from './products/infpl/infpl.component';
+import { VeniteComponent } from './products/venite/venite.component';
+import { WaresComponent } from './products/wares/wares.component';
+
+const AppRoutes: Routes = [
+  { path: 'kariera', component: CareerComponent},
+  { path: '', redirectTo: '/glowna', pathMatch: 'full'},
+  { path: 'glowna', component: MainPageComponent},
+  { path: 'glowna/:id', component: MainPageComponent},
+  { path: 'mucount', component: MucountComponent},
+  { path: 'mucount/:id', component: MucountComponent},
+  { path: 'infpl', component: InfplComponent},
+  { path: 'infpl/:id', component: InfplComponent},
+  { path: 'wildfish', component: WildfishComponent},
+  { path: 'wildfish/:id', component: WildfishComponent},
+  { path: 'software', component: SoftwareComponent},
+  { path: 'software/:id', component: SoftwareComponent},
+  { path: 'sensory', component: SensorsComponent},
+  { path: 'sensory/:id', component: SensorsComponent},
+  { path: 'venite', component: VeniteComponent},
+  { path: 'venite/:id', component: VeniteComponent},
+  { path: 'wares', component: WaresComponent},
+  { path: 'wares/:id', component: WaresComponent},
+  { path: 'praca', component: WorkComponent},
+  { path: 'zapytania_ofertowe', component: QueryRequestComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainPageComponent,
+    CareerComponent,
+    WorkComponent,
+    QueryRequestComponent,
+    ProductsComponent,
+    MucountComponent,
+    WildfishComponent,
+    SensorsComponent,
+    SoftwareComponent,
+    InfplComponent,
+    VeniteComponent,
+    WaresComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CollapseModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    RouterModule.forRoot( AppRoutes )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
